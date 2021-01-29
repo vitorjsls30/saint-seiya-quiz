d43ef0f40e43fa2bc0a24002318863f9dd0fd341
vitor José <vitorjsls30@gmail.com>
2021-01-29 13:37:47 -0300

    main - Declaring base Quiz Structure

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,49 @@
 import styled from 'styled-components'
+import Footer from '../components/Footer';
+import QuizBackground from '../components/QuizBackground';
+import QuizLogo from '../components/QuizLogo';
+import Widget from '../components/Widget';
+import GitHubCorner from '../components/GitHubCorner';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import db from '../db.json';
+
+export const QuizContainer = styled.div`
+  widht: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground>
+      <QuizContainer>
+
+        <QuizLogo />
+
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <p>Quizes da Galera</p>
+            <p>Lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/vitorjsls30/saint-seiya-quiz"/>
+    </QuizBackground>
+  );
 }
