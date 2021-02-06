4358b6a25bb511fc31058dba680e3d12fddeaabe
vitor José <vitorjsls30@gmail.com>
2021-02-06 12:04:05 -0300

    quz-01 - Extracting the Question Alternatives into a new Component

---
+++ src/components/Widget/WidgetAlterrnatives/index.js
@@ -0,0 +1,47 @@
+/* eslint-disable react/no-array-index-key */
+import React from 'react';
+
+import Widget from '../index';
+
+function WidgetAlternatives({
+  alternatives,
+  questionIdx,
+  questionAnswer,
+  handleSelected,
+  isSubmited,
+  selectedValue,
+}) {
+  const shoulddFill = (selected, itemIdx, answer) => {
+    const isCorrect = itemIdx === answer;
+    const isSelected = selected === itemIdx;
+
+    if (isCorrect) return isCorrect;
+
+    if (isSelected) {
+      return isCorrect;
+    }
+
+    return undefined;
+  };
+
+  const items = alternatives.map((item, idx) => (
+    <Widget.Topic
+      as="label"
+      htmlFor={`item__${idx}`}
+      key={`item__${idx}`}
+      data-iscorrect={shoulddFill(selectedValue, idx, questionAnswer)}
+      data-issubmited={isSubmited}
+    >
+      <input
+        type="radio"
+        id={`item_${idx}`}
+        name={`qestion_${questionIdx}`}
+        onClick={() => handleSelected(idx)}
+      />
+      {item}
+    </Widget.Topic>
+  ));
+  return items;
+}
+
+export default WidgetAlternatives;
